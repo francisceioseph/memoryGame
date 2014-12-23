5c7ff79d0be04251a9b54775c877049063571d07
Francisco José A. C. Souza <francisceioseph@gmail.com>
2014-12-23 18:09:20 -0300

    Criacao de um singleton para objetos compartilhados

---
+++ src/sample/Singleton.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.scene.Scene;
+
+/**
+ * Created by gladyson on 23/12/2014.
+ */
+public enum Singleton {
+    INSTANCE;
+    public Scene scene;
+    public int myPort;
+    public int partnerPort;
+}
