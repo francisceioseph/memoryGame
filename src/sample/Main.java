9e9bf26ee0dc7e5b8351b619c3c93bf5ca0e7403
Francisco José A. C. Souza <francisceioseph@gmail.com>
2014-12-29 13:28:01 -0300

    Criacao do método bootIMServer para levantar o servidor de mensagens.
    Criação do método setup para configurar os endereços de rede do servidor local e seu respectivo número de porta.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,31 +1,63 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import sample.communication.IMServerRunnable;
 
 import javax.swing.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Main extends Application {
+    private static IMServerRunnable messagesServerRunnable;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Singleton.INSTANCE.scene = new Scene(root);
-        Singleton.INSTANCE.myPort = Integer.parseInt(JOptionPane.showInputDialog(null, "Porta do meu Server"));
-        Singleton.INSTANCE.partnerPort = Integer.parseInt(JOptionPane.showInputDialog(null, "Porta do Server do coleguinha"));
 
-        Thread t = new Thread(new IMServerRunnable(Singleton.INSTANCE.myPort));
-        t.start();
+        setup();
+        bootIMServer();
+
+        Singleton.INSTANCE.opponentIPAddress = JOptionPane.showInputDialog(null, "Endereço IP do Oponente");
+        Singleton.INSTANCE.opponentIMServerPort = Integer.parseInt(JOptionPane.showInputDialog(null, "Número de Porta do Oponente"));
 
         primaryStage.setTitle("Jogo da Memoria");
         primaryStage.setScene(Singleton.INSTANCE.scene);
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                messagesServerRunnable.setAlive(false);
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
+
+    private static void setup(){
+        try {
+            Singleton.INSTANCE.localIPAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            Singleton.INSTANCE.localIPAddress = "127.0.0.1";
+        }
+
+        Singleton.INSTANCE.localIMServerPort = 7777;
+    }
+    private static void bootIMServer(){
+        messagesServerRunnable = new IMServerRunnable();
+        Thread messagesServerThread = new Thread(messagesServerRunnable);
+        messagesServerThread.start();
+    }
 
     public static void main(String[] args) {
         launch(args);
